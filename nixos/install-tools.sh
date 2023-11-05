b38ca7358562c9912de1dacbc7cb62e4e0396df4
Gabriel Nützi <gnuetzi@gmail.com>
2023-11-05 20:23:48 +0100

    fix: Correct scripts

--- nixos/install-tools.sh
+++ nixos/install-tools.sh
@@ -11,16 +11,16 @@ DIR=$(cd "$(dirname "$0")" && pwd)
 export NIXPKGS_ALLOW_UNFREE=1
 
 nix-env -iA \
-	nixos.gitFull \
-	nixos.lazygit \
-	nixos.curl \
-	nixos.neovim \
-	nixos.wezterm \
-	nixos.tmux \
-	nixos.clang \
-	nixos.google-chrome \
-	nixos.chezmoi \
-	nixos.zsh
+    nixos.gitFull \
+    nixos.lazygit \
+    nixos.curl \
+    nixos.neovim \
+    nixos.wezterm \
+    nixos.tmux \
+    nixos.clang \
+    nixos.google-chrome \
+    nixos.chezmoi \
+    nixos.zsh
 
 git config --global user.name "$GIT_USER_NAME"
 git config --global user.email "$GIT_USER_EMAIL"
@@ -28,12 +28,12 @@ git config --global credential.helper "cache --timeout 72000"
 
 # shellcheck disable=SC2153
 if [ "$DOTFILES_ENABLE" = "true" ]; then
-	chezmoi init https://github.com/gabyx/chezmoi.git
-	chezmoi apply
+    chezmoi init https://github.com/gabyx/chezmoi.git
+    chezmoi apply
 fi
 
 # Install my AstroNvim config
-if [ "$NVIM_ASTRO_ENABLE" = "true" ] ; then
-	[ -d ~/.config/nvim ] || git clone --depth 1 --branch nightly https://github.com/AstroNvim/AstroNvim ~/.config/nvim
-	[ -d ~/.config/nvim/lua/user ] || git clone "$NVIM_ASTRO_USER_URL" ~/.config/nvim/lua/user
+if [ "$NVIM_ASTRO_ENABLE" = "true" ]; then
+    [ -d ~/.config/nvim ] || git clone --depth 1 --branch nightly https://github.com/AstroNvim/AstroNvim ~/.config/nvim
+    [ -d ~/.config/nvim/lua/user ] || git clone "$NVIM_ASTRO_USER_URL" ~/.config/nvim/lua/user
 fi
--- nixos/rebuild-nixos.sh
+++ nixos/rebuild-nixos.sh
@@ -18,11 +18,11 @@ config="-vm"
 export NIXPGKS_ALLOW_INSECURE=1
 
 if [ "$force" = "true" ]; then
-  echo "Rebuild with '$type' system '$config' (default boot entry)."
-	sudo nixos-rebuild -I "nixos-config=./configuration$config.nix" \
-		"$@" "$type"
+    echo "Rebuild with '$type' system '$config' (default boot entry)."
+    sudo nixos-rebuild -I "nixos-config=./configuration$config.nix" \
+        "$@" "$type"
 else
-  echo "Rebuild with '$type' system '$config' with boot entry name 'test'."
-	sudo nixos-rebuild -I "nixos-config=./configuration$config.nix" \
-		"$@" "$type" -p test
+    echo "Rebuild with '$type' system '$config' with boot entry name 'test'."
+    sudo nixos-rebuild -I "nixos-config=./configuration$config.nix" \
+        "$@" "$type" -p test
 fi
