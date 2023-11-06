bde27a8db013c3aac394f06ec3b0882872030f29
Gabriel Nützi <gnuetzi@gmail.com>
2023-11-06 15:56:26 +0100

    fix: Correct sway config

--- config/dot_config/sway/scripts/executable_start-up.sh
+++ config/dot_config/sway/scripts/executable_start-up.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC1091
-set -e
-set -u
+# set -e
+# set -u
 
-# if [ ! -f ~/python-envs/sway/bin/activate ]; then
-#     python3 -m venv ~/python-envs/sway
-#     source ~/python-envs/sway/bin/activate
-#     pip install i3ipc
-# else
-#     source ~/python-envs/sway/bin/activate
-# fi
+LOG=~/.sway-startup.log
+
+echo "Start" > $LOG
+
+# Save stdout and stderr to file 
+exec 3>&1 4>&2 >"$LOG" 2>&1
+
+echo "Starting copyq."
+# waybar &
+copyq --start-server
 
 # Startup when launching sway.
 echo "Starting mail."
@@ -22,3 +25,9 @@ sleep 3
 
 echo "Starting comm."
 swaymsg "workspace '7 Comm'; exec signal-desktop;"
+
+echo "Waiting"
+# Block script
+cat
+
+echo "Finished"
