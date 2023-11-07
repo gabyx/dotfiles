2fcd7da23ef1d5fc03407b03322eb5a83b463d58
Gabriel Nützi <gnuetzi@gmail.com>
2023-11-08 00:11:44 +0100

    fix: Correct network and wireless menus

--- config/dot_config/rofi/executable_show-network-menu.sh
+++ config/dot_config/rofi/executable_show-network-menu.sh
@@ -2,4 +2,10 @@
 
 DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
-"$DIR/scripts/nmcli-rofi.sh"
+if [ "$1" = "--wireless" ]; then
+    "$DIR/scripts/nmcli-rofi.sh"
+elif [ "$1" = "--editor" ]; then
+    nm-connection-editor
+else
+    echo "No command given." >2
+fi
