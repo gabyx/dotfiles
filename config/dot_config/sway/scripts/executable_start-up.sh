b5cf1d5cb868a1faa60f92010138929823cb880d
Gabriel Nützi <gnuetzi@gmail.com>
2023-11-10 11:44:54 +0100

    fix: Keyboard layout etc.

--- config/dot_config/sway/scripts/executable_start-up.sh
+++ config/dot_config/sway/scripts/executable_start-up.sh
@@ -7,7 +7,7 @@ LOG=~/.sway-startup.log
 
 echo "Start" > $LOG
 
-# Save stdout and stderr to file 
+# Save stdout and stderr to file
 exec 3>&1 4>&2 >"$LOG" 2>&1
 
 sleep 1
--- config/dot_config/wezterm/wezterm.lua
+++ config/dot_config/wezterm/wezterm.lua
@@ -26,22 +26,19 @@ config.font_size = 12
 config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
 
-config.debug_key_events = false -- Start `wezterm start --always-new-process` to see the keys
+config.enable_kitty_keyboard = true
+config.debug_key_events = true -- Start `wezterm start --always-new-process` to see the keys
 config.disable_default_key_bindings = true
 
 config.leader = { key = "n", mods = "CTRL", timeout_milliseconds = 1000 }
 config.keys = {
     -- Copy
-    { key = "C", mods = "CTRL", action = act.CopyTo("Clipboard") },
-    { key = "C", mods = "SHIFT|CTRL", action = act.CopyTo("Clipboard") },
     { key = "c", mods = "SHIFT|CTRL", action = act.CopyTo("Clipboard") },
-    { key = "c", mods = "SUPER", action = act.CopyTo("Clipboard") },
+    -- { key = "c", mods = "SUPER", action = act.CopyTo("Clipboard") },
 
     -- Paste
-    { key = "V", mods = "CTRL", action = act.PasteFrom("Clipboard") },
-    { key = "V", mods = "SHIFT|CTRL", action = act.PasteFrom("Clipboard") },
     { key = "v", mods = "SHIFT|CTRL", action = act.PasteFrom("Clipboard") },
-    { key = "v", mods = "SUPER", action = act.PasteFrom("Clipboard") },
+    -- { key = "v", mods = "SUPER", action = act.PasteFrom("Clipboard") },
 
     -- Leader stuff
     { key = "p", mods = "LEADER", action = act.ActivateCommandPalette },
