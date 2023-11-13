dd201817a8779a09af2a74eca51721b7744f5164
Gabriel Nützi <gnuetzi@gmail.com>
2023-11-13 18:30:05 +0100

    fix: Add some tablet settings

--- nixos/modules/keyboard.nix
+++ nixos/modules/keyboard.nix
@@ -6,6 +6,10 @@
   ### Keyboard Settings =================================================
   i18n.defaultLocale = "en_US.UTF-8";
 
+  console = {
+    keyMap = "us";
+  };
+
   services.xserver = {
     layout = "programmer";
     xkbVariant = "";
@@ -23,13 +27,19 @@
     enableGraphical = true;
   };
 
+  # Tablets
+  hardware.opentabletdriver.enable = true;
+
   environment.systemPackages = with pkgs; [
+    # Logitech
     solaar
     xorg.xmodmap
+
+    # Tablets (Wacom)
+    libwacom
+    wacomtablet
+    # xf86_input_wacom #Only on non-wayland.
   ];
 
-  console = {
-    keyMap = "us";
-  };
   # ===========================================================================
 }
