0b45f8aa4cd22aee4a99ab1a2be4fad4543db2cb
Gabriel Nützi <gnuetzi@gmail.com>
2023-11-20 15:50:58 +0100

    fix: Dot files and update systems

--- config/dot_config/zsh/dot_p10k.zsh
+++ config/dot_config/zsh/dot_p10k.zsh
@@ -719,7 +719,7 @@
   # typeset -g POWERLEVEL9K_NIX_SHELL_CONTENT_EXPANSION=
 
   # Custom icon.
-  # typeset -g POWERLEVEL9K_NIX_SHELL_VISUAL_IDENTIFIER_EXPANSION='⭐'
+  typeset -g POWERLEVEL9K_NIX_SHELL_VISUAL_IDENTIFIER_EXPANSION='🚀'
 
   ##################################[ disk_usage: disk usage ]##################################
   # Colors for different levels of disk usage.
