d623940f094d38e430cb9e1262cd995b2b7069dd
Gabriel Nützi <gnuetzi@gmail.com>
2023-11-24 13:32:24 +0100

    fix: Configs

--- config/run_after_install-terminfo.sh
+++ config/run_after_install-terminfo.sh
@@ -3,21 +3,15 @@
 set -e
 set -u
 
-if [ "$CHEZMOI_OS_RELEASE_ID" = "nixos" ]; then
-    echo "WARNING: Terminfo addtionals is not going to be installed in NixOS." >&2
-    echo "         They are installed in NixOS configuration." >&2
-    exit 0
-fi
-
 if [ "$CHEZMOI_OS" = "linux" ] || [ "$CHEZMOI_OS" = "darwin" ]; then
     true
 else
     echo "Architecture '$CHEZMOI_OS' not supported."
 fi
 
-terminfo=~"/.config/wezterm/wezterm.terminfo"
-if [ -f "$terminfo" ] && command -v "tic" &>/dev/null; then
-    tic -x -o ~/.terminfo "$terminfo"
+file=~/.config/wezterm/wezterm.terminfo
+if [ -f "$file" ] && command -v "tic" &>/dev/null; then
+    tic -x -o ~/.terminfo "$file"
     echo "Installed 'wezterm.terminfo' into '~/.terminfo'."
 else
     echo "WARNING: Cannot install 'wezterm.terminfo':" >&2
