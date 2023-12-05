5fb1cd11a5189babfcf61cfacb37bc044e0ed3f8
Gabriel Nützi <gnuetzi@gmail.com>
2023-12-05 18:30:56 +0100

    fix: Browser and kube and k3s

--- config/dot_config/shell/functions.bash
+++ config/dot_config/shell/functions.bash
@@ -28,6 +28,31 @@ function gabyx::bitwarden() {
     export BW_SESSION
 }
 
+function gabyx::get_k3s_killall_script() {
+    local temp
+    temp=$(mktemp)
+
+    curl -sL https://raw.githubusercontent.com/smartin77/create-k3s-killall/main/create-k3s-killall.sh -o "$temp"
+    if [ "$(sha256sum "$temp" | cut -d ' ' -f 1)" != "b93e95218beb47cf37ca4313e61d0fab26514aa168e8aea610a21a04f5780a8e" ]; then
+        echo "SHA sum of script does not match!" >&2
+        return 1
+    fi
+    cd ~/.config/kube || return 1
+
+    echo "Writing 'k3s-killall.sh' to '~/.config/kube'"
+    bash "$temp"
+
+    rm -f "$temp"
+}
+
+function gabyx::k3s_killall() {
+    if [ ! -f ~/.config/kube/k3s-killall.sh ]; then
+        gabyx::get_k3s_killall_script
+    fi
+
+    ~/.config/kube/k3s-killall.sh
+}
+
 function gabyx::remove_docker_images() {
     local builder="docker"
 
