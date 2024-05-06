9a6cf951f267055c99c81b38be2d2902bad3391b
Gabriel Nützi <gnuetzi@gmail.com>
2024-05-06 09:57:07 +0200

    fix: Rofi menu stuff

--- config/dot_config/rofi/executable_show-power-menu.sh
+++ config/dot_config/rofi/executable_show-power-menu.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 
-DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
-
-rofi -show powermenu \
-     -modi "powermenu:$DIR/scripts/rofi-power-menu.sh --symbols-font 'JetBrainsMono Nerd Font'"
-
+rofi -show menu \
+    -modi "menu:rofi-power-menu --symbols-font 'JetBrainsMono Nerd Font'"
