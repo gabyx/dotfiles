f0465cfb64cce534c94297207dee591173dbde2e
Gabriel Nuetzi <gnuetzi@gmail.com>
2024-06-06 23:45:31 +0200

    fix: Waybar styling improvements for laptop displays

--- config/dot_config/waybar/scripts/executable_vpn-status.sh
+++ config/dot_config/waybar/scripts/executable_vpn-status.sh
@@ -6,10 +6,10 @@ vpn=$(nmcli --fields NAME,TYPE connection show --active | grep "vpn")
 if [ "$vpn" != "" ]; then
     name=$(echo "$vpn" | cut -d ' ' -f 1)
     cat <<EOF
-{"tooltip":"$name","class":"connected","percentage":100}
+{"tooltip":"VPN: $name","class":"connected","percentage":100}
 EOF
 else
     cat <<EOF
-{"tooltip":"Disconnected","class":"disconnected","percentage":0}
+{"tooltip":"VPN Disconnected","class":"disconnected","percentage":0}
 EOF
 fi
