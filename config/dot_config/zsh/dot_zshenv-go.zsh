19348846640db1ba0a4f5e4e76295bf6dc5a540a
Gabriel Nuetzi <gnuetzi@gmail.com>
2024-07-25 19:59:22 +0200

    fix: Go path

--- config/dot_config/zsh/dot_zshenv-go.zsh
+++ config/dot_config/zsh/dot_zshenv-go.zsh
@@ -4,5 +4,7 @@ test -d "$GOPATH" || mkdir -p "$GOPATH"
 if [ "$CHEZMOI_OS" = "darwin" ] &&
     brew --prefix golang &>/dev/null; then
     export GOROOT="$(brew --prefix golang)/libexec"
-    export PATH="$GOPATH/bin:$GOROOT/bin:$PATH"
+    export PATH="$GOROOT/bin:$PATH"
 fi
+
+export PATH="$GOPATH/bin:$PATH"
