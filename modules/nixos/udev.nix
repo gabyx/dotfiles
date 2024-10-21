1673b499007b121f532bb692a7e7728df28dc64f
Gabriel Nützi <gnuetzi@gmail.com>
2024-10-21 09:07:21 +0200

    fix: udev rule to start backup (wip)

---
+++ modules/nixos/udev.nix
@@ -0,0 +1,11 @@
+{
+  config,
+  pkgs,
+  ...
+}:
+{
+  # Use `udevadm info -a -n /dev/sde1` to find relevant matching criteria.
+  system.udev.extraRules = ''
+    ACTION="add", SUBSYSTEM=="block", ATTRS{idVendor}=="0781", ATTRS{idProduct}=="55ae",  ATTRS{manufacturer}=="SanDisk", ATTRS{model}=="Extreme 55AE*", ENV{HOME}="${config.settings.user.home}", RUN+="/home/nixos/.config/restic/backup.sh --non-interactive"
+  '';
+}
