61dc45888ac65e9ce91608d49b72611053bccf7b
Gabriel Nützi <gnuetzi@gmail.com>
2024-12-28 18:35:22 +0100

    fix: to 24.11

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
 
   inputs = {
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     # You can access packages and modules from different nixpkgs revs
     # at the same time. Here's an working example:
     nixpkgsStable.url = "github:nixos/nixpkgs/nixos-24.11";
@@ -37,7 +37,7 @@
 
     # Home-Manager for NixOS.
     home-manager = {
-      url = "github:nix-community/home-manager/master";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
