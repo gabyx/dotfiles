ea7e90b5a8a22d86fc4a4e6ec7a026ff5d12f20a
Gabriel Nützi <gnuetzi@gmail.com>
2025-01-01 14:08:21 +0100

    feat: add account profile picture

--- modules/nixos/user.nix
+++ modules/nixos/user.nix
@@ -3,10 +3,14 @@
   pkgs,
   ...
 }:
+let
+  settings = config.settings;
+  userName = settings.user.name;
+in
 {
   ### User Settings ==========================================================
   users = {
-    users.${config.settings.user.name} = {
+    users.${userName} = {
       shell = pkgs.zsh;
 
       useDefaultShell = false;
@@ -46,6 +50,29 @@
 
       openssh.authorizedKeys.keyFiles = [ ../../config/private_dot_ssh/gabyx_ed25519.pub ];
     };
+
   };
+
+  # Place a user image if there is a `.face` (needs to be PNG) in the
+  # user folder.
+  system.activationScripts.script.text = ''
+    mkdir -p /var/lib/AccountsService/{icons,users}
+    pic="${config.settings.user.profilePicture}"
+
+    if [ ! -f "$pic" ]; then
+      echo "User image not existing in '$pic' -> Skip setup."
+      exit 0
+    fi
+
+    echo "User image existing in '$pic' -> Setup."
+    cp "$pic" /var/lib/AccountsService/icons/${userName}
+    echo -e "[User]\nIcon=/var/lib/AccountsService/icons/${userName}\n" > /var/lib/AccountsService/users/${userName}
+
+    chown root:root /var/lib/AccountsService/users/${userName}
+    chmod 0600 /var/lib/AccountsService/users/${userName}
+
+    chown root:root /var/lib/AccountsService/icons/${userName}
+    chmod 0444 /var/lib/AccountsService/icons/${userName}
+  '';
   # ===========================================================================
 }
--- nixos/hosts/settings.nix
+++ nixos/hosts/settings.nix
@@ -1,12 +1,15 @@
 # My own settings to build the NixOS configurations.
-{ lib, ... }:
+{ lib, config, ... }:
+let
+  setts = config.settings;
+in
 with lib;
 {
   # Some option declarations which can be used to specify
   # in `config.settings.???`
   options = {
     settings = {
-      user = rec {
+      user = {
         name = mkOption {
           description = "The main user.";
           default = "nixos";
@@ -15,8 +18,20 @@ with lib;
 
         home = mkOption {
           description = "The home folder.";
-          default = "/home/${name}";
-          type = types.str;
+          default = "/home/${setts.user.name}";
+          type = types.oneOf [
+            types.str
+            types.path
+          ];
+        };
+
+        profilePicture = mkOption {
+          description = "The profile picture in PNG to use. (you can set it to a )";
+          default = "/home/${setts.user.name}/.config/profile-icons/nixos.png";
+          type = types.oneOf [
+            types.str
+            types.path
+          ];
         };
       };
     };
