9364491c16eb4b4ebdbf138daae2f888d7cb13aa
Gabriel Nützi <gnuetzi@gmail.com>
2025-08-22 10:51:15 +0200

    fix: cursor theming

--- nix/home/display.nix
+++ nix/home/display.nix
@@ -5,8 +5,12 @@
 }:
 {
   home.pointerCursor = {
+    x11.enable = true;
+    gtk.enable = true;
     package = pkgs.apple-cursor;
     name = "macOS-Monterey";
     size = 32;
   };
+
+  gtk.enable = true;
 }
