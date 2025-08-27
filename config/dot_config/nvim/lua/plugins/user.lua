ffe17d5ff86c858d5f018b0e4559353c5f6eb3f2
Gabriel Nützi <gnuetzi@gmail.com>
2025-08-27 09:06:33 +0200

    fix: add emoji completion

--- config/dot_config/nvim/lua/plugins/user.lua
+++ config/dot_config/nvim/lua/plugins/user.lua
@@ -208,4 +208,30 @@ return {
             },
         },
     },
+    -- Adding emoji completion.
+    {
+        "moyiz/blink-emoji.nvim",
+        lazy = true,
+        specs = {
+            {
+                "Saghen/blink.cmp",
+                optional = true,
+                opts = {
+                    sources = {
+                        -- enable the provider by default (automatically extends when merging tables)
+                        default = { "emoji" },
+                        providers = {
+                            -- Specific details depend on the blink source
+                            emoji = {
+                                name = "Emoji",
+                                module = "blink-emoji",
+                                min_keyword_length = 1,
+                                score_offset = -1,
+                            },
+                        },
+                    },
+                },
+            },
+        },
+    },
 }
