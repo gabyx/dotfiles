a6545a8a499eace0fbf50f63098f38f2a676092a
Gabriel Nützi <gnuetzi@gmail.com>
2025-10-16 08:40:50 +0200

    fix: run in focused cwd

--- config/dot_config/sway/scripts/executable_run-in-focused-cwd.sh
+++ config/dot_config/sway/scripts/executable_run-in-focused-cwd.sh
@@ -2,7 +2,16 @@
 
 # Select the focused window and get its PID and
 # change to the PIDs working dir.
-focused=$(swaymsg -t get_tree | jq '.. | select(.focused)? | .pid')
-cd -P "/proc/$(pgrep -P "$focused" | head -n 1)/cwd" || true
+focused=$(swaymsg -t get_tree | jq '.. | select(.focused)? | .pid') || {
+    echo "No focused window to get cwd from." >&2
+}
+
+cwd="/proc/$(pgrep -P "$focused" | head -n 1)/cwd" || {
+    echo "No parent process found for '$focused'" >&2
+}
+
+if [ -d "$cwd" ]; then
+    cd -P "$cwd" || true
+fi
 
 exec "$@"
