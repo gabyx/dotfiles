fcbea0cc13305e9e8cdaf777a3a96e96c5ff7344
Gabriel Nützi <gnuetzi@gmail.com>
2025-12-27 19:02:40 +0100

    fix: small qemu vm stuff

--- nix/hosts/vm/qemu.nix
+++ nix/hosts/vm/qemu.nix
@@ -1,16 +1,23 @@
 { ... }:
 {
-  virtualisation.cores = 8;
-  virtualisation.memorySize = 5000;
 
-  virtualisation.qemu = {
-    options = [
-      "-device virtio-gpu"
-      "-display gtk,gl=on"
-      # "-device usb-tablet"
-      # "-spice port=5900,addr=127.0.0.1,disable-ticketing=on"
-      # "-device qxl"
-      # "-vga qxl"
-    ];
+  services = {
+    spice-vdagentd.enable = true;
+  };
+
+  virtualisation = {
+    cores = 8;
+    memorySize = 5000;
+
+    qemu = {
+      options = [
+        "-device virtio-gpu"
+        # "-display gtk,gl=on"
+        # "-device usb-tablet"
+        # "-spice port=5900,addr=127.0.0.1,disable-ticketing=on"
+        # "-device qxl"
+        # "-vga qxl"
+      ];
+    };
   };
 }
