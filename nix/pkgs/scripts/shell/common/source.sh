082e57722271f3df45cd06d98ce01abba65a6236
Gabriel Nützi <gnuetzi@gmail.com>
2026-01-31 17:58:34 +0100

    fix: start new browser script

--- nix/pkgs/scripts/shell/common/source.sh
+++ nix/pkgs/scripts/shell/common/source.sh
@@ -4,6 +4,7 @@
 if [ "${GABYX_LIB_COMMON:-}" != "loaded" ]; then
     source "$GABYX_LIB_DIR/common/log.sh"
     source "$GABYX_LIB_DIR/common/secrets.sh"
+    source "$GABYX_LIB_DIR/common/browser.sh"
 
     GABYX_LIB_COMMON=loaded
 fi
