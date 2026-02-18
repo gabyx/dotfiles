f850abea565cfaf863b1c8813b3db9d2f049f95d
Gabriel Nützi <gnuetzi@gmail.com>
2026-02-18 20:22:03 +0100

    fix: update locks

--- nix/nixos/user-icon.nix
+++ nix/nixos/user-icon.nix
@@ -1,4 +1,6 @@
 {
+  lib,
+  pkgs,
   config,
   ...
 }:
@@ -17,25 +19,30 @@ in
       Group = "root";
     };
 
-    script = ''
-      set -eu
-      mkdir -p /var/lib/AccountsService/{icons,users}
-      pic="${icon}"
+    script =
+      # bash
+      ''
+        set -eu
+        mkdir -p /var/lib/AccountsService/{icons,users}
+        pic="${icon}"
 
-      if [ ! -f "$pic" ]; then
-        echo "User image not existing in '$pic' -> Skip setup."
-        exit 0
-      fi
+        if [ ! -f "$pic" ]; then
+          echo "User image not existing in '$pic' -> Skip setup."
+          exit 0
+        elif "${lib.getExe pkgs.file}" "$pic" | "${lib.getExe pkgs.gnugrep}" -q "ASCII"; then
+          echo "User image is probably a Git LFS object, skipping."
+          exit 1
+        fi
 
-      echo "User image existing in '$pic' -> Setup."
-      cp "$pic" /var/lib/AccountsService/icons/${userName}
-      echo -e "[User]\nIcon=/var/lib/AccountsService/icons/${userName}\n" > /var/lib/AccountsService/users/${userName}
+        echo "User image existing in '$pic' -> Setup."
+        cp "$pic" /var/lib/AccountsService/icons/${userName}
+        echo -e "[User]\nIcon=/var/lib/AccountsService/icons/${userName}\n" > /var/lib/AccountsService/users/${userName}
 
-      chown root:root /var/lib/AccountsService/users/${userName}
-      chmod 0600 /var/lib/AccountsService/users/${userName}
+        chown root:root /var/lib/AccountsService/users/${userName}
+        chmod 0600 /var/lib/AccountsService/users/${userName}
 
-      chown root:root /var/lib/AccountsService/icons/${userName}
-      chmod 0444 /var/lib/AccountsService/icons/${userName}
-    '';
+        chown root:root /var/lib/AccountsService/icons/${userName}
+        chmod 0444 /var/lib/AccountsService/icons/${userName}
+      '';
   };
 }
