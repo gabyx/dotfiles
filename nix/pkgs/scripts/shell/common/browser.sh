88c33d57c419389f690d7a8143ff9a578e9b54b7
Gabriel Nützi <gnuetzi@gmail.com>
2026-06-04 11:45:52 +0200

    fix: shell script

--- nix/pkgs/scripts/shell/common/browser.sh
+++ nix/pkgs/scripts/shell/common/browser.sh
@@ -2,7 +2,7 @@
 # shellcheck disable=SC1090,SC1091,SC2034
 
 function gabyx::start_new_browser() {
-    google-chrome-stable \
-        --user-data-dir="${XDG_CONFIG_DIR:-$HOME/.config}/google-chrome/VPN" >/dev/null 2>&1 &
+    zen-browser \
+        --profile "${XDG_CONFIG_DIR:-$HOME/.config}/zen/VPN" >/dev/null 2>&1 &
     disown
 }
