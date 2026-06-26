bdcbed014305befb51a09a2d6075ecbc4c764728
Gabriel Nützi <gnuetzi@gmail.com>
2026-06-26 17:20:53 +0200

    fix(nvim): add markdown preview plugins

--- nix/pkgs/nvim-nvf/modules/markdown.nix
+++ nix/pkgs/nvim-nvf/modules/markdown.nix
@@ -1,6 +1,21 @@
-{ ... }:
+{ pkgs, ... }:
 {
-  vim.languages.markdown.extensions.markview-nvim = {
-    enable = true;
+
+  # Inline NVIM render
+  vim.lazy.plugins."markview.nvim" = {
+    package = pkgs.vimPlugins.markview-nvim;
+
+    lazy = true;
+    ft = [ "markdown" ];
   };
+
+  vim.lazy.plugins."markdown-preview.nvim" = {
+    package = pkgs.vimPlugins.markdown-preview-nvim;
+
+    lazy = true;
+    ft = [ "markdown" ];
+  };
+  vim.extraPackages = [
+    pkgs.nodejs-slim_latest
+  ];
 }
