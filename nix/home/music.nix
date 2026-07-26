7af97c2e1d254f8dc60595326274f29300947582
Gabriel Nützi <gnuetzi@gmail.com>
2026-07-26 13:33:02 +0200

    fix(music): wrong neural amp modeler link

--- nix/home/music.nix
+++ nix/home/music.nix
@@ -5,6 +5,6 @@
 }:
 {
   home.file.".lv2/neural-amp-modeler" = {
-    source = outputs.packages.${system}.neural-amp-modeler-lv2;
+    source = "${outputs.packages.${system}.neural-amp-modeler-lv2}/lib/lv2/neural_amp_modeler.lv2";
   };
 }
